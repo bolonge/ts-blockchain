7fdaec0e686b9bb946fc6d34506e8966f7675f9d
bolonge <xxs456@naver.com>
2019-04-09 16:23:52 +0900

    #2 first block

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
-};
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+        ){
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
+    }
+}
 
-console.log(sayHi("bo", 24, "male"));
+const gensisBlock:Block = new Block(0,"1235462184","","hello",123456);
+
+let blockchain: [Block] = [gensisBlock];
+
+console.log(blockchain);
